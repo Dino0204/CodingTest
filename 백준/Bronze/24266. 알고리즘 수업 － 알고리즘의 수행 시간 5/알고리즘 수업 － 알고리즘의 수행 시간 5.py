2ae4b99459816c495e2b258f8a299cfd9a70db7d
김준혁 <162967089+Dino0204@users.noreply.github.com>
2024-12-10 14:12:56 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 5, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/24266. 알고리즘 수업 － 알고리즘의 수행 시간 5/알고리즘 수업 － 알고리즘의 수행 시간 5.py
@@ -0,0 +1,12 @@
+# MenOfPassion
+# sum = 0
+# for i in range(1,n+1):
+#   for j in range(1,n+1):
+#     for k in range(1,n+1):
+#       sum += A[i] * A[j] * A[k]
+# return sum
+
+# 시간복잡도 O(n^3)
+n = int(input())
+print(n*n*n)
+print(3)
