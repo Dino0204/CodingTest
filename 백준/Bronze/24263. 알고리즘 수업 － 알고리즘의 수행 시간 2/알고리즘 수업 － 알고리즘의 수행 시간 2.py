624221cc4a71884a71f40459371d63b992a3b8d9
김준혁 <162967089+Dino0204@users.noreply.github.com>
2024-12-10 14:00:23 +0900

    [Bronze IV] Title: 알고리즘 수업 - 알고리즘의 수행 시간 2, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/24263. 알고리즘 수업 － 알고리즘의 수행 시간 2/알고리즘 수업 － 알고리즘의 수행 시간 2.py
+++ 백준/Bronze/24263. 알고리즘 수업 － 알고리즘의 수행 시간 2/알고리즘 수업 － 알고리즘의 수행 시간 2.py
@@ -1,12 +1,10 @@
-# A = [1 for _ in range(n)]
+# MenOfPassion
 # sum = 0
 # for i in range(1,n+1):
 #   sum += A[i]
 # print(sum)
 
-# 1 ~ n인덱스까지의 값을 더하고 반환
-# 시간 복잡도 n번
-
+# 시간 복잡도 O(n)
 n = int(input())
 print(n)
 print(1)
