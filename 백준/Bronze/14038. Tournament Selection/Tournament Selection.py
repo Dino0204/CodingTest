97d6acdb67fbd2dff7f8c1d4f36bf3d7e137a344
김준혁 <162967089+Dino0204@users.noreply.github.com>
2025-10-02 16:03:41 +0900

    [Bronze IV] Title: Tournament Selection, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/14038. Tournament Selection/Tournament Selection.py
@@ -0,0 +1,15 @@
+cnt = 0
+for i in range(6):
+  result = input()
+  
+  if result == "W":
+    cnt += 1
+
+if cnt >= 5:
+  print(1)
+elif cnt >= 3:
+  print(2)
+elif cnt  >= 1:
+  print(3)
+else:
+  print(-1)
